772e82ee13b97e264b997e077ff2e1c1cf50c224
coderkan <erkanguzeler@gmail.com>
2019-01-03 23:17:58 +0300

    updated karma files.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,14 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    // you can define custom flags
-    customLaunchers: {
-    	ChromeHeadlessNoSandbox: {
-		base: 'ChromeHeadless',
-		flags: ['--no-sandbox']
-	}
-    },
-    singleRun: false
+	browsers: ['ChromeHeadless'],
+		// you can define custom flags
+		customLaunchers: {
+			ChromeHeadlessNoSandbox: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			}
+	},
+    singleRun: true
   });
 };
