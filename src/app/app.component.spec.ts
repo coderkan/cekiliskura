43131adb34402c67e7ece646ca0da32b3a062cad
coderkan <erkanguzeler@gmail.com>
2019-01-06 21:11:26 +0300

    added angular material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,19 +5,35 @@ import { HeaderComponent } from '../app/components/header/header.component';
 import { FooterComponent } from '../app/components/footer/footer.component';
 import { AdvertisementComponent } from '../app/components/advertisement/advertisement.component';
 import { InputItemComponent } from '../app/components/input-item/input-item.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule, MatAutocompleteModule } from '@angular/material/';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+ 
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatChipsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatOptionModule,
+        MatAutocompleteModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         AppComponent,
         HeaderComponent,
         FooterComponent,
         AdvertisementComponent,
-        InputItemComponent
+        InputItemComponent,
+      
       ],
     }).compileComponents();
   }));
--- src/app/components/input-item/input-item.component.spec.ts
+++ src/app/components/input-item/input-item.component.spec.ts
@@ -2,12 +2,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputItemComponent } from './input-item.component';
 
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule, MatAutocompleteModule } from '@angular/material/';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 describe('InputItemComponent', () => {
   let component: InputItemComponent;
   let fixture: ComponentFixture<InputItemComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatChipsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatOptionModule,
+        MatAutocompleteModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
       declarations: [ InputItemComponent ]
     })
     .compileComponents();
