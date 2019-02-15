f8708682c5b9cd5c520f7eec25829be5e849b6bb
coderkan <erkanguzeler@gmail.com>
2019-02-16 00:39:33 +0300

    added RouterTestingModule to test cases.

--- src/app/components/draw-lots/draw-lots.component.spec.ts
+++ src/app/components/draw-lots/draw-lots.component.spec.ts
@@ -1,13 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DrawLotsComponent } from './draw-lots.component';
- 
+import { RouterTestingModule } from '@angular/router/testing';
+
 describe('DrawLotsComponent', () => {
   let component: DrawLotsComponent;
   let fixture: ComponentFixture<DrawLotsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [ DrawLotsComponent ]
     })
     .compileComponents();
